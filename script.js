37513d6ae174333138da7f09b51cce87a6d7c7a3
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-11-07 13:56:15 -0300

    create player factory function

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
-// You’re going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects… and you’re probably going to want an object to control the flow of the game itself.
-
-//     Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory. Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
-
-// Set up your HTML and write a JavaScript function that will render the contents of the gameboard array to the webpage (for now you can just manually fill in the array with "X"s and "O"s)
-
+//   //Select all html tiles 
+//   const tiles = document.querySelectorAll("[data-tile]");
+//   //Add listener for clicks(one per clicked tile) on each tile
+//   tiles.forEach(tile => {
+//     tile.addEventListener("click", e, { once: true });
+//     console.log(tile)
+//   });
 
 
 // function e() {
 //   console.log("click")
 // }
+  
 
 const game = (() => {
-// let e = () => {
-
-// }
-  // const tiles = document.querySelectorAll("[data-tile]");
-  // tiles.forEach(tile => {
-  //  tile.addEventListener("click", gameBoard, {once: true});
-  // });
-
-  // gameBoard.push(e)
 
   const gameBoard = () => {
+
+    //Select all html tiles 
     const tiles = document.querySelectorAll("[data-tile]");
+    //Add listener for clicks(one per clicked tile) on each tile
     tiles.forEach(tile => {
-      tile.addEventListener("click", { once: true });
+      tile.addEventListener("click", render, { once: true });
     });
 
-    const arrayOfTiles = Array.from(tiles); //converts nodelist to array 
-    console.log(arrayOfTiles);
-    
-    // arrayOfTiles.push(tiles);
+    //converts nodelist to array 
+    const arrayOfTiles = Array.from(tiles); 
 
+    console.log(arrayOfTiles);
 
   };
-    // const players = {
-      
-    // }
-    
-    // const gameControl = e => {
-  
-    //   console.log(e);
-    // }
+  const render = () => {
+    console.log("click")
+    arrayOfTiles.innerText = "X"
+
+  }
+
     return {
-      gameBoard
+      gameBoard,
+      render
     }
 })()
 
-
 const player = (name, shape) => {
-  const getName = name
-  const getShape = shape 
+  const getName  = () => name; 
+  const getShape = () => shape;
+
+  return {getName, getShape}
 }
+
+const player1 = player("player1", "X") //works
+const player2 = player("player2", "O")
+
+// console.log(player1.getName())
+// console.log(player1.getShape())
+// console.log(player2.getName())
+// console.log(player2.getShape())
