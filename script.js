a22f4d9ac75923edafd7fc5f1f42d2e89823f44c
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-11-07 20:32:15 -0300

    tiles can be marked now

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ const game = (() => {
     const tiles = document.querySelectorAll("[data-tile]");
     //Add listener for clicks(one per clicked tile) on each tile
     tiles.forEach(tile => {
-      tile.addEventListener("click", render, { once: true });
+      tile.addEventListener("click", (e) => {
+        putMark(e.target)}, { once: true });
     });
 
     //converts nodelist to array 
@@ -29,16 +30,21 @@ const game = (() => {
     console.log(arrayOfTiles);
 
   };
-  const render = () => {
-    console.log("click")
-    arrayOfTiles.innerText = "X"
 
+  function putMark(e) {
+
+    console.log(e)
+    let target = e
+    let text = "X"
+    target.innerText = text
+    
   }
 
     return {
       gameBoard,
-      render
+      putMark
     }
+
 })()
 
 const player = (name, shape) => {
@@ -55,3 +61,4 @@ const player2 = player("player2", "O")
 // console.log(player1.getShape())
 // console.log(player2.getName())
 // console.log(player2.getShape())
+game.gameBoard()
